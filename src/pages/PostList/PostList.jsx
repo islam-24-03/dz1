2b3a2277e9cb2087ff657c93860a8e5a727be11e
islam-24-03 <isabhnisa@gmail.com>
2023-06-26 23:29:43 +0600

    first commit

---
+++ src/pages/PostList/PostList.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import '../../App.css';
+
+export default function PostList() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => setPosts(response.data));
+
+        return (
+            <div>
+                <h4>Posts</h4>
+                <ul>
+                    {posts.map(p => (
+                        <li key={p.id}>
+                            <Link to={`/${p.id}`}>{p.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+                <Link to="/create">
+                    <p>Create Post</p>
+                </Link>
+            </div>
+        );
+    }
